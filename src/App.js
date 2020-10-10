f222d6ba28580cfe37466615985c2b51de450859
Dak <67485000+Dmelcher16@users.noreply.github.com>
2020-10-09 23:38:41 -0700

    updated page

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import SaveBooks from "./pages/SaveBooks";
+import SearchBooks from "./pages/SearchBooks";
+import Nav from "./components/Nav";
+import "./App.css"
+
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={SearchBooks} />
+          <Route exact path="/saved" component={SaveBooks} />
+          <Route exact path="/saved/:id" component={SaveBooks} /> 
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
